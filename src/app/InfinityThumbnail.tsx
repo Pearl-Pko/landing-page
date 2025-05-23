b51d207e7d7f7608ec6a5dda5b707030a62b729f
Pearl-Pko <osamuede75@gmail.com>
2025-05-23 12:24:06 +0100

    feat: add two more layers in the marquee group

--- src/app/InfinityThumbnail.tsx
+++ src/app/InfinityThumbnail.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import React from "react";
 import { motion } from "motion/react";
 
-const images = [
+const contents = [
   {
     images: [
       "https://droip.com/wp-content/uploads/2025/03/Template-Thumbnail-1.webp",
@@ -11,8 +11,37 @@ const images = [
       "https://droip.com/wp-content/uploads/2025/03/Template-Thumbnail-4.webp",
     ],
 
-    width: "420px",
-    height: "236px",
+    width: 420,
+    height: 236,
+    duration: 25,
+  },
+  {
+    images: [
+      "https://droip.com/wp-content/uploads/2025/03/Template-Thumbnail-8.webp",
+      "https://droip.com/wp-content/uploads/2025/03/Template-Thumbnail.webp",
+      "https://droip.com/wp-content/uploads/2025/03/Template-Thumbnail-4.webp",
+      "https://droip.com/wp-content/uploads/2025/03/Template-Thumbnail-7.webp",
+      "https://droip.com/wp-content/uploads/2025/03/Template1-1.webp",
+    ],
+
+    width: 320,
+    height: 180,
+    duration: 10,
+  },
+  {
+    images: [
+      "https://droip.com/wp-content/uploads/2025/03/Template-Thumbnail-9.webp",
+      "https://droip.com/wp-content/uploads/2025/03/Template-Thumbnail-11.webp",
+      "https://droip.com/wp-content/uploads/2025/03/Template-Thumbnail-6.webp",
+      "https://droip.com/wp-content/uploads/2025/03/Template-Thumbnail-12.webp",
+      "https://droip.com/wp-content/uploads/2025/03/Template-Thumbnail-12-1.webp",
+      "https://droip.com/wp-content/uploads/2025/03/Template4-1.webp",
+      "https://droip.com/wp-content/uploads/2025/03/Template8.webp",
+    ],
+
+    width: 240,
+    height: 135,
+    duration: 25,
   },
 ];
 
@@ -20,17 +49,20 @@ const MarqueeContent = ({
   images,
   width,
   height,
+  keyPrefix,
 }: {
   images: string[];
-  width: string;
-  height: string;
+  keyPrefix: string;
+  width: number;
+  height: number;
 }) => {
   return (
     <>
       {images.map((image, index) => (
         <div
-          key={index}
-          className={`inline-block relative w-[420px] h-[236px] mr-[4px]`}
+          key={`${keyPrefix}-${index}`}
+          className={`inline-block relative mr-[4px]`}
+          style={{ width: `${width}px`, height: `${height}px` }}
         >
           <Image
             fill
@@ -56,34 +88,39 @@ export default function InfinityThumbnail() {
         </p>
       </div>
       <div className="absolute h-auto w-[65%]  inset-0 min-w-auto top-[95px] flex overflow-hidden flex-col right-0 left-auto bottom-0 ">
-        <div className="overflow-hidden h-full relative">
-          <motion.div
-            animate={{ x: [0, `-${4 * (420 + 4)}px`] }}
-            transition={{
-              repeat: Infinity,
-              repeatType: "loop",
-              ease: "linear",
-              duration: 10,
-            }}
-            className=" whitespace-nowrap h-full"
-          >
-            <MarqueeContent
-              images={images[0].images}
-              width={images[0].width}
-              height={images[0].height}
-            />
-            <MarqueeContent
-              images={images[0].images}
-              width={images[0].width}
-              height={images[0].height}
-            />
-          </motion.div>
-            <div className="absolute top-0 left-0 bottom-0 w-[120px] bg-gradient-to-r from-black   to-transparent">
+        <div className="overflow-hidden relative h-auto">
+          {contents.map((content, index) => {
+            return (
+              <motion.div
+                animate={{
+                  x: [0, `-${content.images.length * (content.width + 4)}px`],
+                }}
+                transition={{
+                  repeat: Infinity,
+                  repeatType: "loop",
+                  ease: "linear",
+                  duration: content.duration,
+                }}
+                className=" whitespace-nowrap h-auto"
+              >
+                <MarqueeContent
+                  keyPrefix="1"
+                  images={content.images}
+                  width={content.width}
+                  height={content.height}
+                />
+                <MarqueeContent
+                  keyPrefix="2"
+                  images={content.images}
+                  width={content.width}
+                  height={content.height}
+                />
+              </motion.div>
+            );
+          })}
 
-            </div>
-                        <div className="absolute top-0 right-0 bottom-0 w-[120px] bg-gradient-to-l from-black   to-transparent">
-
-            </div>
+          <div className="absolute top-0 left-0 bottom-0 w-[120px] bg-gradient-to-r from-black   to-transparent"></div>
+          <div className="absolute top-0 right-0 bottom-0 w-[120px] bg-gradient-to-l from-black   to-transparent"></div>
         </div>
         {/* 
         <Image
