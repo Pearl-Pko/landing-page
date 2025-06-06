69a4ce1463f238b42646472dd3e4d59ce3448117
Pearl-Pko <osamuede75@gmail.com>
2025-06-06 13:39:08 +0100

    fix: mobile layout for the vertical sliding images

--- src/app/VerticalSlide.tsx
+++ src/app/VerticalSlide.tsx
@@ -56,10 +56,17 @@ export default function VerticalSlide() {
   const markerTarget = useMotionValue(0);
 
   const imageOutlineRef = useRef<HTMLDivElement | null>(null);
-  const targetRef = useRef(null);
+  const desktopTargetRef = useRef(null);
+  const mobileTargetRef = useRef<HTMLDivElement | null>(null);
+
+  const currentRef = useRef<HTMLDivElement | null>(null);
+
+  const [currentLayout, setCurrentLayout] = useState<
+    "mobile" | "desktop" | null
+  >();
 
   const { scrollYProgress } = useScroll({
-    target: targetRef,
+    target: currentRef,
     offset: ["start start", "end end"],
   });
 
@@ -77,6 +84,10 @@ export default function VerticalSlide() {
   });
   const backgroundValue = useTransform(fade, [0, 1], ["#212426", "#000000"]);
   const topValue = useTransform(fade, (v) => v * 300);
+  const jumpStartSectionYValue = useTransform(
+    topValue,
+    (v) => v * -1 * (currentLayout === "desktop" ? 0 : 1)
+  );
   const opacityValue = useTransform(fade, (v) => 1 - v);
 
   const transform = useTransform(
@@ -85,6 +96,10 @@ export default function VerticalSlide() {
     ["0%", "-20%", "-40%", "-60%"]
   );
 
+  const finalTransform = useTransform(() => {
+    const numericValue = parseFloat(transform.get());
+    return `${numericValue - fade.get() * 15}%`;
+  });
   // const transform = useSpring(rawTransform, {
   //   stiffness: 1000,
   //   damping: 100,
@@ -134,8 +149,6 @@ export default function VerticalSlide() {
   useMotionValueEvent(scrollYProgress, "change", (latest) => {
     if (!scrollYProgress.getPrevious()) return;
 
-    console.log("da", latest);
-
     const direction = latest - scrollYProgress.getPrevious()! > 0 ? 1 : -1;
     handleAlign(direction);
   });
@@ -143,6 +156,28 @@ export default function VerticalSlide() {
   useEffect(() => {
     handleAlign(-1);
   }, []);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(min-width: 1024px)");
+
+    const respondToScreenChange = (
+      ev: MediaQueryListEvent | MediaQueryList
+    ) => {
+      if (ev.matches) {
+        currentRef.current = desktopTargetRef.current;
+        setCurrentLayout("desktop");
+      } else {
+        currentRef.current = mobileTargetRef.current;
+        setCurrentLayout("mobile");
+      }
+    };
+
+    if (!currentLayout) respondToScreenChange(mediaQuery);
+
+    mediaQuery.addEventListener("change", respondToScreenChange);
+    return () =>
+      mediaQuery.removeEventListener("change", respondToScreenChange);
+  }, [desktopTargetRef.current, mobileTargetRef.current]);
 
   // useEffect(() => {
   //   // const alignTop = () => {
@@ -172,89 +207,107 @@ export default function VerticalSlide() {
   // }, []);
 
   return (
-    <motion.div style={{ background: backgroundValue }}>
-      <div ref={targetRef} className="mx-auto  max-w-[1330px] h-[200vh] px-4">
-        <div className="lg:sticky lg:top-0 origin-top max-h-[830px] pt-[100px] ">
-          <div className="flex flex-col lg:flex-row gap-16 items-start overflow-hidden">
+    <motion.div className="h-auto" style={{ background: backgroundValue }}>
+      <div
+        ref={desktopTargetRef}
+        className="mx-auto  max-w-[1330px] lg:h-[200vh] px-4"
+      >
+        <div className="lg:sticky lg:top-0 origin-top lg:max-h-[830px] pt-[100px] ">
+          <div className="flex flex-col lg:flex-row lg:gap-16 items-start lg:overflow-hidden">
             {/* <div className="h-screen flex items-center sticky top-0"> */}
-            <SweepWhiteOverlay delay={0.5}>
-              <motion.p
-                style={{ opacity: opacityValue, top: topValue }}
-                className="text-5xl lg:text-8xl break-keep flex flex-col justify-center font-semibold"
-              >
+            <motion.p
+              style={{ opacity: opacityValue, top: topValue }}
+              className="text-5xl lg:text-8xl break-keep flex flex-col justify-center font-semibold"
+            >
+              <SweepWhiteOverlay delay={0.5}>
                 Create <span className="whitespace-nowrap">pixel-perfect</span>{" "}
                 accuracy in the atomic level{" "}
-              </motion.p>
-            </SweepWhiteOverlay>
+              </SweepWhiteOverlay>
+            </motion.p>
             {/* </div> */}
             <div
-              ref={imageSliderRef}
-              className="max-w-[235px] h-full relative flex flex-col items-center "
+              ref={mobileTargetRef}
+              className="relative h-[200vh] mx-auto w-full order-3 lg:order-0 lg:static lg:h-full"
             >
-              <motion.div
-                style={{ y: transform }}
-                className="flex flex-col items-stretch justify-between gap-4"
+              <div
+                ref={imageSliderRef}
+                //  ref={targetRef}
+                // ref={(el) => {
+                //   // imageOutlineRef.current = el;
+                //   // mobileTargetRef.current = el;
+                // }}
+                className="sticky top-0 origin-top pt-[100px] max-w-[235px] mx-auto  max-h-[830px] lg:max-h-none lg:h-full  lg:relative flex flex-col items-center"
               >
-                {cards.map((card, ind) => {
-                  const distance = useTransform(index, (i) =>
-                    Math.abs(i - ind)
-                  );
-                  const width = useTransform(
-                    distance,
-                    [0, 1, 2],
-                    ["100%", "75%", "50%"]
-                  );
-                  return (
-                    <motion.div
-                      ref={(el) => void (imagesRefs.current[ind] = el)}
-                      className="mx-auto my-auto h-auto"
-                      style={{ width: width }}
-                      key={ind}
-                    >
-                      <Image
-                        alt=""
-                        width={900}
-                        height={300}
-                        style={{ objectFit: "contain" }}
-                        src={card.image}
-                      />
-                    </motion.div>
-                  );
-                })}
-              </motion.div>
-              {/* <motion.div
-              style={{ y: markerTarget }}
-              className="absolute origin-top top-0 rounded-full w-4 h-4 bg-red-200"
-            ></motion.div> */}
-              <motion.div
-                ref={imageOutlineRef}
-                // layoutId="shared-layout"
-                style={{
-                  height: imageOutlineHeight,
-                  y: imageOutlineTop,
-                }}
-                className="absolute origin-top top-0 inset-0  w-full  border-dashed border-[3px] border-[#00a0a6]"
-              >
-                <div className="absolute -left-[5px] -top-[5px] h-[8px] w-[8px] border-2 rounded-full border-[#00a0a6] bg-[#212426]"></div>
-                <div className="absolute -right-[5px] -top-[5px] h-[8px] w-[8px] border-2 rounded-full border-[#00a0a6] bg-[#212426]"></div>
-                <div className="absolute -left-[5px] -bottom-[5px] h-[8px] w-[8px] border-2 rounded-full border-[#00a0a6] bg-[#212426]"></div>
-                <div className="absolute -right-[5px] -bottom-[5px] h-[8px] w-[8px] border-2 rounded-full border-[#00a0a6] bg-[#212426]"></div>
-                <div className="absolute top-1/2 -translate-y-1/2 -left-[5px] h-[8px] w-[8px] border-[1px] border-[#585858] bg-[#212426]"></div>
-                <div className="absolute top-1/2 -translate-y-1/2 -right-[5px] h-[8px] w-[8px] border-[1px] border-[#585858] bg-[#212426]"></div>
-              </motion.div>
-              <motion.div
-                style={{ opacity: fade }}
-                className="text-white text-9xl font-bold absolute bottom-[360px]"
-              >
-                Or
-              </motion.div>
+                <div className="overflow-hidden lg:overflow-visible">
+                  <motion.div
+                    style={{ y: finalTransform, opacity: opacityValue }}
+                    className="flex flex-col  items-stretch justify-between gap-4 overflow-hidden"
+                  >
+                    {cards.map((card, ind) => {
+                      const distance = useTransform(index, (i) =>
+                        Math.abs(i - ind)
+                      );
+                      const width = useTransform(
+                        distance,
+                        [0, 1, 2],
+                        ["100%", "75%", "50%"]
+                      );
+                      return (
+                        <motion.div
+                          ref={(el) => void (imagesRefs.current[ind] = el)}
+                          className="mx-auto my-auto h-auto"
+                          style={{ width: width }}
+                          key={ind}
+                        >
+                          <Image
+                            alt=""
+                            width={900}
+                            height={300}
+                            style={{ objectFit: "contain" }}
+                            src={card.image}
+                          />
+                        </motion.div>
+                      );
+                    })}
+                  </motion.div>
+                </div>
+                {/* <motion.div
+                  style={{ y: markerTarget }}
+                  // style={{ opacity: opacityValue, top: topValue }}
+
+                  className="absolute origin-top top-0 rounded-full w-4 h-4 bg-red-200"
+                ></motion.div> */}
+                <motion.div
+                  ref={imageOutlineRef}
+                  // layoutId="shared-layout"
+                  style={{
+                    height: imageOutlineHeight,
+                    y: imageOutlineTop,
+                    opacity: opacityValue,
+                  }}
+                  className="absolute origin-top top-0 inset-0  w-full  border-dashed border-[3px] border-[#00a0a6]"
+                >
+                  <div className="absolute -left-[5px] -top-[5px] h-[8px] w-[8px] border-2 rounded-full border-[#00a0a6] bg-[#212426]"></div>
+                  <div className="absolute -right-[5px] -top-[5px] h-[8px] w-[8px] border-2 rounded-full border-[#00a0a6] bg-[#212426]"></div>
+                  <div className="absolute -left-[5px] -bottom-[5px] h-[8px] w-[8px] border-2 rounded-full border-[#00a0a6] bg-[#212426]"></div>
+                  <div className="absolute -right-[5px] -bottom-[5px] h-[8px] w-[8px] border-2 rounded-full border-[#00a0a6] bg-[#212426]"></div>
+                  <div className="absolute top-1/2 -translate-y-1/2 -left-[5px] h-[8px] w-[8px] border-[1px] border-[#585858] bg-[#212426]"></div>
+                  <div className="absolute top-1/2 -translate-y-1/2 -right-[5px] h-[8px] w-[8px] border-[1px] border-[#585858] bg-[#212426]"></div>
+                </motion.div>
+                <motion.div
+                  style={{ opacity: fade }}
+                  className="text-white text-9xl font-bold absolute bottom-[360px] lg:bottom-[600px]"
+                >
+                  Or
+                </motion.div>
+              </div>
             </div>
             <motion.div
-              className="h-screen flex flex-col mt-[448px]"
+              className="lg:h-screen flex flex-col my-[30px] lg:mt-[448px] lg:mb-0"
               style={{ opacity: opacityValue, top: topValue }}
             >
               <SweepWhiteOverlay delay={0.5}>
-                <p className="text-2xl">
+                <p className="text-lg lg:text-2xl">
                   Create your website from the ground up with complete control
                   over every element, down to the smallest detail
                 </p>
@@ -264,7 +317,10 @@ export default function VerticalSlide() {
           </div>
         </div>
       </div>
-      <div className="max-w-[1330px] mx-auto">
+      <motion.div
+        style={{ marginTop: jumpStartSectionYValue }}
+        className="max-w-[1330px] mx-auto"
+      >
         <div className="grid mx-auto grid-cols-1 lg:grid-cols-2 px-4 lg:px-20 ">
           <p className="text-3xl lg:text-6xl text-white  lg:col-start-2 font-semibold">
             Jumpstart your business with beautifully crafted themes and sections
@@ -273,7 +329,7 @@ export default function VerticalSlide() {
         <div className="py-12 lg:pt-0 lg:pb-32">
           <InfinityThumbnail />
         </div>
-      </div>
+      </motion.div>
     </motion.div>
   );
 }
